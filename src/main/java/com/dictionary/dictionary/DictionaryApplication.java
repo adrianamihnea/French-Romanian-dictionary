2454aa474a0bb37a715cbdd9165d8766e46cadfb
adrianamihnea <126603624+adrianamihnea@users.noreply.github.com>
2024-03-10 21:21:30 +0200

    Implemented CRUD operations; Solved issue #1

--- src/main/java/com/dictionary/dictionary/DictionaryApplication.java
+++ src/main/java/com/dictionary/dictionary/DictionaryApplication.java
@@ -12,6 +12,8 @@ import org.springframework.context.annotation.Bean;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class DictionaryApplication {
@@ -23,14 +25,16 @@ public class DictionaryApplication {
 	@Bean
 	CommandLineRunner init(WordRepository wordRepository, TranslationRepository translationRepository) {
 		return args -> {
+
+			// create
 			Word word1 = new Word(null, "fenetre", null);
 			Word word2 = new Word(null, "porte", null);
-
+			Word word3 = new Word(null, "cerveau", null);
 			ArrayList<Word> words = new ArrayList<>();
 			words.add(word1);
 			words.add(word2);
+			words.add(word3);
 			wordRepository.saveAll(words);
-
 			Translation translation1 = new Translation(null, "fereastra", null);
 			translation1.setWordInFrench(word1);
 			Translation translation2 = new Translation(null, "geam", null);
@@ -49,7 +53,34 @@ public class DictionaryApplication {
 			translationRepository.saveAll(translations);
 			wordRepository.saveAll(words);
 
+			// retrieve
+			Optional<Translation> translation = translationRepository.findById(word1.getId());
+			System.out.println("Translation for word " + word1.getWordInFrench() + " is " + translation.get().getWordInRomanian());
+			System.out.println("Translation for word " + word1.getWordInFrench() + " is " +
+					word1.getTranslations()
+					.stream()
+					.map(Translation::getWordInRomanian)
+					.collect(Collectors.toList())
+			);
 
+			//update
+			String modifiedString = "fenêtre";
+			Iterable<Word> allWords = wordRepository.findAll();
+			for(Word word : allWords) {
+				if(word.getWordInFrench().equals("fenetre")) {
+					word.setWordInFrench(modifiedString);
+					wordRepository.save(word);
+				}
+			}
+
+			// delete
+			String wordToBeDeleted = "cerveau";
+			allWords = wordRepository.findAll();
+			for(Word word : allWords) {
+				if(word.getWordInFrench().equals(wordToBeDeleted)) {
+					wordRepository.delete(word);
+				}
+			}
 		};
 
 	}
